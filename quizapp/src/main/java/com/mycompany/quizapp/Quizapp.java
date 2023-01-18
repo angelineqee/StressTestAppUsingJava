704c6cdbeb28227ef913dae209016c0824b7054a
Angel <angelqq2002@gmail.com>
2023-01-19 00:42:25 +0800

    Start Quiz and Result Page

---
+++ quizapp/src/main/java/com/mycompany/quizapp/Quizapp.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.quizapp;
+
+public class Quizapp {
+
+    public static void main(String[] args) {
+        new StartQuizPage();
+    }
+}
