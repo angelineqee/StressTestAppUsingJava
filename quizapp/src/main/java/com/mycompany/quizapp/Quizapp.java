f7ffbc5b185dba9d4fcd1ccacb210b0ea04aea60
angelineqee <91053468+angelineqee@users.noreply.github.com>
2023-01-22 21:38:40 +0800

    Added Question page design

--- quizapp/src/main/java/com/mycompany/quizapp/Quizapp.java
+++ quizapp/src/main/java/com/mycompany/quizapp/Quizapp.java
@@ -7,7 +7,6 @@ package com.mycompany.quizapp;
 public class Quizapp {
 
     public static void main(String[] args) {
-        Login login = new Login();
-        login.setVisible(true);
+        new Login().setVisible(true);
     }
 }
