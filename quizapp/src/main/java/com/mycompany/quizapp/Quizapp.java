1fb659f63f83017a415b947c406f7379741f68f8
angelineqee <91053468+angelineqee@users.noreply.github.com>
2023-01-22 20:58:54 +0800

    Merge branch 'Danish-latest' into basic-version-master
    Changed frame size

--- quizapp/src/main/java/com/mycompany/quizapp/Quizapp.java
+++ quizapp/src/main/java/com/mycompany/quizapp/Quizapp.java
@@ -7,6 +7,7 @@ package com.mycompany.quizapp;
 public class Quizapp {
 
     public static void main(String[] args) {
-        new StartQuizPage();
+        Login login = new Login();
+        login.setVisible(true);
     }
 }
